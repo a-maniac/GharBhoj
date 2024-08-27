3d2c178edcd6f826f021b4b6f1123759700067ce
amanjoshi <ajoshi373@gmail.com>
2024-08-28 00:32:47 +0530

    defining restaurant entity,service

--- src/main/java/com/example/GharBhoj/GharBhoj/entities/Restaurent.java
+++ src/main/java/com/example/GharBhoj/GharBhoj/entities/Restaurent.java
@@ -1,12 +1,17 @@
 package com.example.GharBhoj.GharBhoj.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Restaurent {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String address;
