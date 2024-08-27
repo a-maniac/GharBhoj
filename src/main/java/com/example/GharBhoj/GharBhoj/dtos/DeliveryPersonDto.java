d75c45f6c617a5ddc8277de10a36543a3923942f
amanjoshi <ajoshi373@gmail.com>
2024-08-28 00:27:41 +0530

    defining delivery person entity,dto and servicef

---
+++ src/main/java/com/example/GharBhoj/GharBhoj/dtos/DeliveryPersonDto.java
@@ -0,0 +1,7 @@
+package com.example.GharBhoj.GharBhoj.dtos;
+
+public class DeliveryPersonDto {
+
+    private UserDto user;
+    private String rating;
+}
--- src/main/java/com/example/GharBhoj/GharBhoj/entities/DeliveryPerson.java
+++ src/main/java/com/example/GharBhoj/GharBhoj/entities/DeliveryPerson.java
@@ -1,9 +1,15 @@
 package com.example.GharBhoj.GharBhoj.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 public class DeliveryPerson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
-    private String phoneNumber;
-    private String currLocation;
+
+    @OneToOne
+    private User user;
 }
